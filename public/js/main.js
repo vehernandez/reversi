3fad95f89bac0c86fb2aed75555276af471cc4c5
VH <vh@VHs-MacBook-Pro.local>
2017-05-23 01:23:34 -0700

    Upload changes to lobby.html, main.js, and server.js. Added chat functionality.

--- public/js/main.js
+++ public/js/main.js
@@ -18,4 +18,41 @@ if('undefined' == typeof username || !username){
 	username = 'Anonymous_'+Math.floor(Math.random()*100);
 }
 
-$('#messages').append('<h4>' +username+ '</h4>');
+var chat_room = 'One_Room';
+
+
+/* Connect to the socket server */
+var socket = io.connect();
+
+socket.on('log', function(array){
+		  console.log.apply(console,array);
+});
+socket.on('join_room_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+	}
+	$('#messages').append('<p>New user joined the room: ' +payload.username+ '</p');
+});
+socket.on('send_message_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+	}
+	$('#messages').append('<p><strong>' +payload.username+ ' says: </strong>' +payload.message+ '</p>');
+});
+function send_message(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	payload.message = $('#send_message_holder').val();
+	console.log('*** Client Log Message: \'send_message\' payload: ' +JSON.stringify(payload));
+	socket.emit('send_message',payload);
+}
+
+$(function(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	
+	console.log('*** Client Log Message: \'join.room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
+});
